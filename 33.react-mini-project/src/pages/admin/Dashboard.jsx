679165614455db23b7899f828660236586b1c777
ClassTime <classtime@code.local>
2026-02-13 13:08:22 +0400

    fix: update Dashboard to use Grid2 with correct import

--- 33.react-mini-project/src/pages/admin/Dashboard.jsx
+++ 33.react-mini-project/src/pages/admin/Dashboard.jsx
@@ -7,7 +7,7 @@ import {
     Button,
     Divider
   } from '@mui/material'
-  import Grid from '@mui/material/Grid2'
+  import Grid from '@mui/material/Grid'
   import { Link } from 'react-router-dom'
   import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
   import ArticleIcon from '@mui/icons-material/Article'
